b03cfe2dac03e70cc543a3091cfa4ed09520b306
González Laureano Gabriel <120410551+GonzalezLaureano@users.noreply.github.com>
2023-06-05 21:10:51 -0300

    Delete Proyecto-Sistemas-Digitales directory

--- Proyecto-Sistemas-Digitales/lib/FirebaseConfiguration/FirebaseConfiguration.cpp
+++
@@ -1,1 +0,0 @@
-//no me dejo hacerlo en dos archivos separados, asi que tuve que poner el codigo en el .H
--- Proyecto-Sistemas-Digitales/lib/WifiEsp8266/WifiEsp8266.h
+++
@@ -1,9 +0,0 @@
-#ifndef __WifiEsp8266_H_
-#define __WifiEsp8266_H_
-#include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include <WifiEsp8266.h>
-#define WIFI_SSID "VideoDigital-Osuna"
-#define WIFI_PASSWORD "osuna5112"
-void WifiConnect();
-#endif
