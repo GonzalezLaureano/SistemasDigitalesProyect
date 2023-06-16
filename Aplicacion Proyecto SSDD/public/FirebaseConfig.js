1e78d403ee8dbaec46f2988d9932102e0e916a22
LAUREANO\laure <laureanogonzalez2020@gmail.com>
2023-06-16 20:58:59 -0300

    no message

---
+++ Aplicacion Proyecto SSDD/public/FirebaseConfig.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC3HZcs3UEEeIacnFXC8zYiAak_zVwBNEw",
+  authDomain: "sistemasdigitalesproyect.firebaseapp.com",
+  projectId: "sistemasdigitalesproyect",
+  storageBucket: "sistemasdigitalesproyect.appspot.com",
+  messagingSenderId: "932015706452",
+  appId: "1:932015706452:web:6933d442f8cb8167aff8c1"
+};
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+// Obtiene una referencia a la ubicación de la base de datos que deseas leer
+var database = firebase.database();
+//holaaa
